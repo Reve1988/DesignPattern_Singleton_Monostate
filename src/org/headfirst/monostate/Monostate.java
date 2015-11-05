492cc07510410c4447cc147dd89ef15575fd2395
giwoong.kim <giwoong.kim@nhn.com>
2015-11-05 13:32:55 +0900

    [NOBTS] generate Singleton, Monostate Code

--- src/org/headfirst/monostate/Monostate.java
+++ src/org/headfirst/monostate/Monostate.java
@@ -1,5 +1,31 @@
 package org.headfirst.monostate;
 
 public class Monostate {
+	private static String string;
+	private static int number;
+	private static boolean bool;
 
+	public String getString() {
+		return string;
+	}
+
+	public void setString(String string) {
+		Monostate.string = string;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		Monostate.number = number;
+	}
+
+	public boolean isBool() {
+		return bool;
+	}
+
+	public void setBool(boolean bool) {
+		Monostate.bool = bool;
+	}
 }
--- src/org/headfirst/singleton/Singleton.java
+++ src/org/headfirst/singleton/Singleton.java
@@ -1,5 +1,17 @@
 package org.headfirst.singleton;
 
 public class Singleton {
+	private static Singleton instance;
 
+	private Singleton() {
+	}
+
+	public static Singleton getInstance() {
+		// 스레드 이슈 및 if성능 이슈
+		if (instance == null) {
+			instance = new Singleton();
+		}
+
+		return instance;
+	}
 }
